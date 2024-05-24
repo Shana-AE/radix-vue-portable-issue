1df3648e570b1882f2c5cb407e5477bcecec45a3
Shana-AE <Shana.wav@gmail.com>
2024-05-24 16:38:18 +0800

    add demo of portable issue of radix-vue

---
+++ src/Foobar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps } from 'radix-vue';
+
+export interface Props extends PrimitiveProps {
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'button',
+});
+
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+  >
+    <slot />
+  </Primitive>
+</template>
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Foo from './Foobar.vue';
+
+const a = Foo as typeof Foo;
+export { a };
+export { Foo };
